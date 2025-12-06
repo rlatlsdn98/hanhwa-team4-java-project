4b989270c270f89dc4d7f40c61db5c04ce26f9d2
sinwooKim <rlatlsdn98@naver.com>
2025-12-06 19:26:54 +0900

    set file path final

--- src/main/java/com/ohgiraffers/configuration/Config.java
+++ src/main/java/com/ohgiraffers/configuration/Config.java
@@ -5,7 +5,7 @@ public enum Config {
     OUTPUT_FILE_PATH("src/main/resources/booklist.txt"),
     INPUT_FILE_PATH("src/main/resources/booklist.txt");
 
-    private String filePath;
+    private final String filePath;
 
     Config(String filePath){
         this.filePath = filePath;
